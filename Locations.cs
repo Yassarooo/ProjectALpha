6f5686019fc9eed1f37b3a6b9fee5fd653692911
Baset-1102300 <1102300@hr.nl>
2026-02-27 14:58:23 +0100

    classes added

--- Locations.cs
+++ Locations.cs
@@ -3,5 +3,11 @@ public class Location
     public int ID;
     public string Name;
     public string Description;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToWest;
+    public Location LocationToSouth;
     
 }
